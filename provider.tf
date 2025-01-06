f2562671c4d03a13025cc4b37d54cfd71b9bb8ae
sachin <sachin@gmail.com>
2025-01-06 10:31:01 +0530

    code adeed from feature branch

--- provider.tf
+++ provider.tf
@@ -2,12 +2,13 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.113.0"
+       version = "4.14.0"
     }
   }
 }
 
 provider "azurerm" {
+  subscription_id = "162a85a7-dadd-45c6-9175-6911af8553a1"
   features {
 
   }
